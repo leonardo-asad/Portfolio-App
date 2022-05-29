34933c59e4f7cee449857071b015953b6de3f2b0
Leonardo Asad <leonardo.asad.89@gmail.com>
2022-05-29 20:54:12 -0300

    asdasd

--- backend/settings.py
+++ backend/settings.py
@@ -143,6 +143,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
