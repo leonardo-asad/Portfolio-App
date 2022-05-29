e152db5a9fe16100ce77bd2bcbbc12ea8903172d
Leonardo Asad <leonardo.asad.89@gmail.com>
2022-05-29 15:46:00 -0300

    asdasd

--- backend/settings.py
+++ backend/settings.py
@@ -142,6 +142,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
