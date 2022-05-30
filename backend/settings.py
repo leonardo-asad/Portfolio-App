7ca65c27ef580c0cf2a1691e1bd9d6f11ef7d158
Leonardo Asad <leonardo.asad.89@gmail.com>
2022-05-30 17:40:32 -0300

    asdasd

--- backend/settings.py
+++ backend/settings.py
@@ -141,7 +141,6 @@ USE_TZ = True
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "RESULT_BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": os.environ.get('REDIS_URL'),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
