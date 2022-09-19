c9b08d685d1e8f286da9ed7b3ba1ff482874dcce
Leonardo Asad <leonardo.asad.89@gmail.com>
2022-09-19 13:38:01 +1200

    asdasd

--- backend/settings.py
+++ backend/settings.py
@@ -168,7 +168,7 @@ CELERYBEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 STATIC_URL = 'static/'
 
-REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend')
+REACT_APP_DIR = os.path.join(BASE_DIR, 'frontendts')
 
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
